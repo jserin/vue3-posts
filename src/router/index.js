87ed9f039e09cb3b9094926cfc3f175f3e2e6b00
jserin <jsr3643@naver.com>
2024-06-21 17:00:24 +0900

    240621

--- src/router/index.js
+++ src/router/index.js
@@ -1,89 +1,73 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '@/views/HomeView.vue';
-import AboutView from '@/views/AboutView.vue';
-import PostCreateView from '@/views/posts/PostCreateView.vue';
-import PostDetailView from '@/views/posts/PostDetailView.vue';
-import PostEditView from '@/views/posts/PostEditView.vue';
-import PostListView from '@/views/posts/PostListView.vue';
-import NotFoundView from '@/views/NotFoundView.vue';
-import NestedView from '@/views/nested/NestedView.vue';
-import NestedOneView from '@/views/nested/NestedOneView.vue';
-import NestedTwoView from '@/views/nested/NestedTwoView.vue';
-import NestedHomeView from '@/views/nested/NestedHomeView.vue';
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
+import PostCreateView from '@/views/posts/PostCreateView.vue'
+import PostDetailView from '@/views/posts/PostDetailView.vue'
+import PostEditView from '@/views/posts/PostEditView.vue'
+import PostListView from '@/views/posts/PostListView.vue'
+import NotFoundView from '@/views/NotFoundView.vue'
+import NestedView from '@/views/nested/NestedView.vue'
+import NestedOneView from '@/views/nested/NestedOneView.vue'
+import NestedTwoView from '@/views/nested/NestedTwoView.vue'
+import NestedHomeView from '@/views/nested/NestedHomeView.vue'
 import MyPage from '@/views/MyPage.vue'
-
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: HomeView,
+    component: HomeView
   },
   {
     path: '/about',
     name: 'About',
-    component: AboutView,
+    component: AboutView
   },
   {
     path: '/posts',
     name: 'PostList',
-    component: PostListView,
+    component: PostListView
   },
   {
     path: '/posts/create',
     name: 'PostCreate',
-    component: PostCreateView,
+    component: PostCreateView
   },
   {
     path: '/posts/:id',
     name: 'PostDetail',
     component: PostDetailView,
-    props: true,
+    props: true
   },
   {
     path: '/posts/:id/edit',
     name: 'PostEdit',
-    component: PostEditView,
+    component: PostEditView
   },
-  { path: '/:pathMatch(.*)*',
-    name: 'NotFound',
-    component: NotFoundView,
-  },
-  { path: '/nested',
+  { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFoundView },
+  {
+    path: '/nested',
     name: 'Nested',
     component: NestedView,
     children: [
-      { path: '',
-        name: 'NestedHome',
-        component: NestedHomeView,
-      },
-      { path: 'one',
-        name: 'NestedOne',
-        component: NestedOneView,
-      },
-      { path: 'two',
-        name: 'NestedTwo',
-        component: NestedTwoView,
-      },
+      { path: '', name: 'NestedHome', component: NestedHomeView },
+      { path: 'one', name: 'NestedOne', component: NestedOneView },
+      { path: 'two', name: 'NestedTwo', component: NestedTwoView }
     ]
   },
-  { path: '/my',
-    name: 'MyPage',
-    component: MyPage,
-    beforeEnter: [removeQueryString]
-  },
-];
+  { path: '/my', name: 'MyPage', component: MyPage, beforeEnter: [removeQueryString] }
+]
 
 function removeQueryString(to) {
   if (Object.keys(to.query).length > 0) {
-    return {path: to.path, query:{}}
+    return { path: to.path, query: {} }
   }
 }
 
 const router = createRouter({
   history: createWebHistory('/'),
-  routes,
-});
+  routes // 키와 값이 같으므로 생략
+})
 
 // router.beforeEach((to, from) => {
 //   if (to.name === 'MyPage') {
@@ -91,4 +75,6 @@ const router = createRouter({
 //     return {name: 'Home'}
 //   }
 // })
-export default router;
+
+// 외부에서 사용할 수 있도록 export
+export default router
--- src/views/posts/PostListView.vue
+++ src/views/posts/PostListView.vue
@@ -1,41 +1,49 @@
 <template>
   <div>
     <h2>게시글 목록</h2>
-    <hr class="my-4">
+    <hr class="my-4" />
     <PostFilter
       v-model:title="params.title_like"
       :limit="params._limit"
       @update:limit="changeLimit"
     />
-    <hr class="my-4">
-    <AppLoading v-if="loading"/>
-    <AppError v-else-if="error" :message="error.message"/>
+    <hr class="my-4" />
+    <AppLoading v-if="loading" />
+    <AppError v-else-if="error" :message="error.message" />
 
     <template v-else-if="!isExist">
       <p class="text-center py-5 text-muted">No Results</p>
     </template>
     <template v-else>
       <AppGrid :items="posts" col-class="col-12 col-md-6 col-lg-4">
-        <template v-slot="{item}">
-          <PostItem :title="item.title" :content="item.content" :created-at="item.createdAt"
+        <template v-slot="{ item }">
+          <PostItem
+            :title="item.title"
+            :content="item.content"
+            :created-at="item.createdAt"
             @click="goPage(item.id)"
             @modal="openModal(item)"
             @preview="selectPreview(item.id)"
           ></PostItem>
         </template>
       </AppGrid>
-      <AppPagination 
-        :current-page="params._page" 
-        :page-count="pageCount" 
-        @page="page => params._page = page"
+      <AppPagination
+        :current-page="params._page"
+        :page-count="pageCount"
+        @page="(page) => (params._page = page)"
       />
     </template>
     <Teleport to="#modal">
-      <PostModal v-model="show" :title="modalTitle" :content="modalContent" :create-at="modalCreatedAt"></PostModal>
+      <PostModal
+        v-model="show"
+        :title="modalTitle"
+        :content="modalContent"
+        :create-at="modalCreatedAt"
+      ></PostModal>
     </Teleport>
 
     <template v-if="previewId">
-      <hr class="my-4">
+      <hr class="my-4" />
       <AppCard>
         <PostDetailView :id="previewId"></PostDetailView>
       </AppCard>
@@ -44,19 +52,19 @@
 </template>
 
 <script setup>
-import PostItem from '@/components/posts/PostItem.vue';
-import PostDetailView from '@/views/posts/PostDetailView.vue';
-import PostFilter from '@/components/posts/PostFilter.vue';
-import PostModal from '@/components/posts/PostModal.vue';
+import PostItem from '@/components/posts/PostItem.vue'
+import PostDetailView from '@/views/posts/PostDetailView.vue'
+import PostFilter from '@/components/posts/PostFilter.vue'
+import PostModal from '@/components/posts/PostModal.vue'
 
-import { computed, ref } from 'vue';
-import { useRouter } from 'vue-router';
-import { useAxios } from '@/hooks/useAxios';
+import { computed, ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useAxios } from '@/hooks/useAxios'
 
-const router = useRouter();
+const router = useRouter()
 
-const previewId = ref(null);
-const selectPreview = id => previewId.value = id;
+const previewId = ref(null)
+const selectPreview = (id) => (previewId.value = id)
 
 // pagination
 const params = ref({
@@ -64,44 +72,42 @@ const params = ref({
   _order: 'desc',
   _page: 1,
   _limit: 6,
-  title_like: '',
-});
+  title_like: ''
+})
 
-const changeLimit = value => {
-  params.value._limit = value;
-  params.value._page = 1;
+const changeLimit = (value) => {
+  params.value._limit = value
+  params.value._page = 1
 }
 
-const { response, data: posts, error, loading } = useAxios('/posts', { params });
+const { response, data: posts, error, loading } = useAxios('/posts', { params })
 
 const isExist = computed(() => posts.value && posts.value.length > 0)
-const totalCount = computed(() => response.value.headers['x-total-count']);
-const pageCount = computed(() => Math.ceil(totalCount.value / params.value._limit));
+const totalCount = computed(() => response.value.headers['x-total-count'])
+const pageCount = computed(() => Math.ceil(totalCount.value / params.value._limit))
 
-const goPage = id => {
+const goPage = (id) => {
   // router.push(`/posts/${id}`);
   router.push({
     name: 'PostDetail',
     params: {
-      id,
+      id
     }
   })
 }
 
 //modal
-const show = ref(false);
-const modalTitle = ref('');
-const modalContent = ref('');
-const modalCreatedAt = ref('');
+const show = ref(false)
+const modalTitle = ref('')
+const modalContent = ref('')
+const modalCreatedAt = ref('')
 
 const openModal = ({ title, content, createdAt }) => {
-  show.value = true;
-  modalTitle.value = title;
-  modalContent.value = content;
-  modalCreatedAt.value = createdAt;
+  show.value = true
+  modalTitle.value = title
+  modalContent.value = content
+  modalCreatedAt.value = createdAt
 }
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
